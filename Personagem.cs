2278edbdc8a495ba71b21e8e4cc73f3ba5aa5b62
Felipe Marins <marins.sgs@gmail.com>
2021-04-20 21:28:31 -0300

    Modificar ComprarItem

--- Personagem.cs
+++ Personagem.cs
@@ -9,17 +9,54 @@ public class Personagem
 
     public Loja ComprarItem(string nomeDoItem, int quantidade, Loja lojaAtual)
     {
-        if (quantidade > 1)
+        if (lojaAtual.ItemExiste(nomeDoItem))
         {
-            for (int i = 0; i < quantidade; i++)
+            if (lojaAtual.RetornarQuantidadeItem(nomeDoItem) >= quantidade)
             {
-                this.Inventario.Add(lojaAtual.VenderItem(nomeDoItem));
+                Item itemSelecionado = lojaAtual.RetornarItem(nomeDoItem);
+                int valorTotal = itemSelecionado.Valor * quantidade;
+                if (valorTotal <= Ouro)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Item: " + itemSelecionado.Nome);
+                    Console.WriteLine("Valor: " + itemSelecionado.Valor);
+                    Console.WriteLine("Unidades a se comprar: " + quantidade);
+                    Console.WriteLine("Valor total: " + valorTotal);
+
+                    Menu confirmacaoDeCompra = new Menu(
+						"Item: " + itemSelecionado.Nome +
+                        "\nValor: " + itemSelecionado.Valor +
+                        "\nUnidades a se comprar: " + quantidade +
+                        "\nValor total: " + valorTotal +
+                        "\n\nTem certeza de que deseja realizar esta compra?",
+                        new List<string> { "Sim", "Não" });
+                    confirmacaoDeCompra.IniciarMenuPadrao();
+                    if (confirmacaoDeCompra.Selecao == 1)
+                    {
+                        if (quantidade > 1)
+                        {
+                            for (int i = 0; i < quantidade; i++)
+                            {
+                                this.Inventario.Add(lojaAtual.VenderItem(nomeDoItem));
+                            }
+                        }
+                        else if (quantidade == 1)
+                        {
+                            this.Inventario.Add(lojaAtual.VenderItem(nomeDoItem));
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Você não tem dinheiro suficiente para comprar esse item.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("A loja não tem " + quantidade + " unidades desse item.");
             }
         }
-        else if (quantidade == 1)
-        {
-            this.Inventario.Add(lojaAtual.VenderItem(nomeDoItem));
-        }
+        Menu.EsperarPorTecla();
         return lojaAtual;
     }
 }
